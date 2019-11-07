5b3213753992cfc757667e13d4d30496b347e5d0
HellPwnage1337 <hellpwnage1337@gmail.com>
2019-11-07 13:40:42 -0600

    finished normalization

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ basic parsing program for reading a text
 
 import numpy as np
 import math
+import glob
 
 def sanitize(word):
     #forbidden = ["a","an","as","but","or","and","for","is","was","be","the","so","at"]
@@ -16,6 +17,21 @@ def sanitize(word):
     #if word in forbidden :
      #   return -1
     return word
+
+def normalize(tfidf):
+    Ntfidf = tfidf
+    for x in range(0,len(tfidf)):
+        print(tfidf[x])
+        square = []
+        for i in range(0,len(tfidf[0])):
+            square.append(math.pow(tfidf[x][i],2))
+        total = math.sqrt(sum(square))
+        print(total)
+        for y in range(0,len(tfidf[0])):
+            Ntfidf[x][y] = round(tfidf[x][y] / total,3)
+
+    return Ntfidf
+
 
 
 def termfrequency(index, docs):
@@ -55,7 +71,7 @@ def tf_idf(tf, idf):
         for y in range(0,len(tf[0])):
 
                 tfidf[x][y] = round((tf[x][y] * idf[y]),3)
-    print(tfidf)
+    return tfidf
 
 
 def parse(text):
@@ -66,11 +82,13 @@ def parse(text):
     return tokens
 
 
-addr = ["./example_docs/doc1.txt", "./example_docs/doc2.txt","./example_docs/doc3.txt","./example_docs/doc4.txt"]
+#addr = ["./example_docs/doc1.txt", "./example_docs/doc2.txt","./example_docs/doc3.txt","./example_docs/doc4.txt"]
 docs = []
 tokens = []
 counts = []
 tf = []
+addr = glob.glob("./docs/*.txt")
+print(addr)
 for a in addr:
     file =open(a, "r")
     docs.append(file.read())
@@ -88,4 +106,7 @@ print(index)
 tf = termfrequency(index, docs)
 
 idf = docfrequency(tf,len(docs))
-tf_idf(tf,idf)
+tfidf = tf_idf(tf,idf)
+print(tfidf)
+Ntfidf=normalize(tfidf)
+print(Ntfidf)
