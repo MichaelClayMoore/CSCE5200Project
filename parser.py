aca75375951e22fdcbbbfb86db3b5eff0e3fa17c
HellPwnage1337 <hellpwnage1337@gmail.com>
2019-11-11 18:08:16 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -8,58 +8,60 @@ import glob
 
 
 def sanitize(word):
-    #forbidden = ["a","an","as","but","or","and","for","is","was","be","the","so","at"]
+    # forbidden = ["a","an","as","but","or","and","for","is","was","be","the","so","at"]
     if word.endswith("\'s"):
         word = word[:len(word) - 2]
     if word.endswith('\'') or word.endswith('.') or word.endswith('?') or word.endswith('!') or word.endswith('\"') or word.endswith(','):
         word = word[:len(word)-1]
     if word[0] == '\'' or word[0]=='\"':
         word = word[1:]
-    #if word in forbidden :
+    # if word in forbidden :
      #   return -1
     return word
 
 
 def normalize(tfidf):
     Ntfidf = tfidf
-    for x in range(0,len(tfidf)):
-        #print(tfidf[x])
+    for x in range(0, len(tfidf)):
+        # print(tfidf[x])
         square = []
-        for i in range(0,len(tfidf[0])):
+        for i in range(0, len(tfidf[0])):
             square.append(math.pow(tfidf[x][i],2))
         total = math.sqrt(sum(square))
-        #print(total)
-        for y in range(0,len(tfidf[0])):
+        # print(total)
+        for y in range(0, len(tfidf[0])):
             if total > 0:
-               Ntfidf[x][y] = round(tfidf[x][y] / total,3)
+               Ntfidf[x][y] = round(tfidf[x][y] / total, 3)
             else:
                 Ntfidf[x][y] = 0
 
     return Ntfidf
 
+
 def Qnormalize(Qtfidf):
     QNtfidf = [None] * len(Qtfidf)
     total = 0
-    for x in range(0,len(Qtfidf)):
-        total += math.pow(Qtfidf[x][0],2)
-    for x in range(0,len(Qtfidf)):
-        QNtfidf[x]= Qtfidf[x][0]/math.sqrt(total)
+    for x in range(0, len(Qtfidf)):
+        total += math.pow(Qtfidf[x][0], 2)
+    for x in range(0, len(Qtfidf)):
+        QNtfidf[x] = Qtfidf[x][0]/math.sqrt(total)
     return QNtfidf
 
 
 def Qtermfrequency(index, query):
     tf = [None] * len(index)
-    for i in range(0,len(tf)):
-        tf[i]=[]
-        tf[i].append(round(query.count(index[i]),3))
+    for i in range(0, len(tf)):
+        tf[i] = []
+        tf[i].append(round(query.count(index[i]), 3))
     return tf
+
 
 def termfrequency(index, docs):
     tf = [None] * len(docs)
     #print(index)
     for d in range(0,len(docs)):
         words = docs[d].split()
-        for w in range(0,len(words)):
+        for w in range(0, len(words)):
             words[w] = sanitize(words[w].lower())
      #   print(words)
         tf[d] = []
@@ -69,28 +71,30 @@ def termfrequency(index, docs):
             if count == 0:
                 tf[d].append(0)
             else:
-                log = 1 + math.log(count,10)
+                log = 1 + math.log(count, 10)
 
                 tf[d].append(log)
     return tf
 
-def docfrequency(count,N):
+
+def docfrequency(count, N):
     idf = []
 
     for y in range(0,len(count[0])):
         df = 0
-        for x in range(0,len(count)):
+        for x in range(0, len(count)):
             if count[x][y] != 0:
                 df += 1
         idf.append(round(math.log((N/df), 10),3))
     return idf
 
+
 def tf_idf(tf, idf):
-    tfidf=tf
+    tfidf = tf
     for x in range(0,len(tf)):
         for y in range(0,len(tf[0])):
 
-                tfidf[x][y] = round((tf[x][y] * idf[y]),3)
+                tfidf[x][y] = round((tf[x][y] * idf[y]), 3)
     return tfidf
 
 
@@ -102,30 +106,32 @@ def parse(text):
     return tokens
 
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
-addr = ["./example_docs/doc1.txt", "./example_docs/doc2.txt","./example_docs/doc3.txt"]
-=======
+# addr = ["./example_docs/doc1.txt", "./example_docs/doc2.txt","./example_docs/doc3.txt"]
+
+# =======
 def cosine(norm, Qnorm):
     sim = []
-    for x in range(0,len(norm)):
+    for x in range(0, len(norm)):
         total = 0
-        for y in range(0,len(norm[0])):
+        for y in range(0, len(norm[0])):
             total += norm[x][y] * Qnorm[y]
-        sim.append(round(total,3))
+        sim.append(round(total, 3))
     return sim
 
-#addr = ["./example_docs/doc1.txt", "./example_docs/doc2.txt","./example_docs/doc3.txt","./example_docs/doc4.txt"]
->>>>>>> 90e6dc59687137bf7d1d3d71f34258aa6d774029
+# addr = ["./example_docs/doc1.txt", "./example_docs/doc2.txt","./example_docs/doc3.txt","./example_docs/doc4.txt"]
+# >>>>>>> 90e6dc59687137bf7d1d3d71f34258aa6d774029
+
 docs = []
 tokens = []
-Qtokens=[]
+Qtokens = []
 counts = []
 tf = []
 addr = glob.glob("./docs/*.txt")
 query = "reaction parameters"
 for a in addr:
-    file =open(a, "r")
+    file = open(a, "r")
     docs.append(file.read())
 
 for d in range(0, len(docs)):
@@ -141,12 +147,18 @@ print(index)
 tf = termfrequency(index, docs)
 Qtf = Qtermfrequency(index, query)
 print(Qtf)
-idf = docfrequency(tf,len(docs))
-tfidf = tf_idf(tf,idf)
-Qtfidf = tf_idf(Qtf,idf)
+idf = docfrequency(tf, len(docs))
+tfidf = tf_idf(tf, idf)
+Qtfidf = tf_idf(Qtf, idf)
 print(Qtfidf)
 Ntfidf=normalize(tfidf)
 QNtfidf = Qnormalize(Qtfidf)
 print(QNtfidf)
-sim = cosine(Ntfidf,QNtfidf)
+sim = cosine(Ntfidf, QNtfidf)
 print(sim)
+ans = []
+for i in range(0, len(docs)):
+    d = sim.index(max(sim))
+    ans.append(addr[d])
+    sim.remove(max(sim))
+print(ans)
